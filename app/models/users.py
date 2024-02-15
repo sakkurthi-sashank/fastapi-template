e553b011457121bce67e9205069ad6c2aedbd970
sakkurthi-sashank <sakkurthisashank@gmail.com>
2024-02-15 23:41:51 +0530

    [fix] users model default values

--- app/models/users.py
+++ app/models/users.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, TIMESTAMP
+from sqlalchemy import Column, String, TIMESTAMP, text
 from sqlalchemy.sql import func
 from ..config.db import Base
 
@@ -7,12 +7,26 @@ class Users(Base):
 
     __tablename__ = "users"
 
-    id = Column("id", String, primary_key=True, unique=True, nullable=False)
+    id = Column(
+        "id",
+        String,
+        default=text("gen_random_uuid()"),
+        primary_key=True,
+        unique=True,
+        nullable=False,
+    )
     display_name = Column("display_name", String(255))
     email = Column("email", String(255), unique=True, nullable=False, index=True)
     photo_url = Column("photo_url", String(255), nullable=True)
 
     update_at = Column(
-        "updated_at", TIMESTAMP(timezone=True), default=func.now(), onupdate=func.now()
+        "updated_at",
+        TIMESTAMP(timezone=True),
+        default=text("now()"),
+        onupdate=text("now()"),
     )
-    created_at = Column("created_at", TIMESTAMP(timezone=True), default=func.now())
+    created_at = Column(
+        "created_at",
+        TIMESTAMP(timezone=True),
+        default=text("now()"),
+    )
